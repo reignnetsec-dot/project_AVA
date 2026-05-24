9685d39767252b0522059a3826edc3b7ce0f99ba
reignnetsec-dot <reignnetsec@gmail.com>
2026-05-24 15:36:11 +0200

    up2

--- modules/pass_manager.py
+++ modules/pass_manager.py
@@ -1,10 +1,39 @@
 import pandas as pd
+import random
 
 
 class PassMan:
-    def __init__(self, username: str, password: str):
-        self.username = username
-        self.password = password
+    def __init__(self):
+        # self.username = username
+        # self.password = password
+        pass
+
+
+    def _generate_password(self, strength: int, chars: dict):
+        lower_case = chars['lower_case']
+        upper_case = chars['upper_case']
+        digits = chars['digits']
+        punctuation = chars['punctuation']
+
+        if strength == 1:
+            # weak password: random lowercase + digits
+            password = []
+            for _ in range(5):
+                lower_case_char = random.choice(lower_case)
+                password.append(lower_case_char)
+            for _ in range(5):
+                digit = random.choice(digits)
+                password.append(digit)
+            return ''.join(password)
+        
+        elif strength == 2:
+            # mid password: random lowercase + uppercase + digits
+            ...
+        elif strength == 3:
+            # strong password: random lowercase + uppsercase + digits + symbols
+            ...
+        else:
+            ...
 
 
     def _to_csv(self, username, password):
@@ -19,7 +48,25 @@ class PassMan:
 
 
 if __name__ == "__main__":
-    username_input = input("USERNAME: ")
-    password_input = input("PASSWORD: ")
-    pass_man = PassMan(username_input, password_input)
-    pass_man._to_csv(username_input, password_input)
+    
+    pass_man = PassMan()
+
+    chars = {
+            # Lowercase letters
+            'lower_case': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+
+            # Uppercase letters
+            'upper_case': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+
+            # Digits
+            'digits': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+
+            # Punctuation & symbols (the 32 printable symbols on US QWERTY)
+            'punctuation': ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+               '-', '_', '=', '+', '[', ']', '{', '}', '\\', '|', ';', ':',
+               "'", '"', ',', '<', '.', '>', '/', '?']
+            }
+
+    print(pass_man._generate_password(1, chars))
